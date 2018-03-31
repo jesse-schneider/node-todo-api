c48e557951d5c715c8e973cef6e06d185579d69d
Jesse Schneider <jessewschneider96@gmail.com>
2018-03-31 13:08:38 +1000

    add DELETE /todos/:id route

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,17 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+
+Todo.remove({}).then((res) => {
+  console.log(res);
+});
+
+
+//Todo.findOneAndRemove()
+
+Todo.findByIdAndRemove('5abef75951702b3113a5e8c0').then((todo) => {
+  console.log(todo);
+});
--- server/server.js
+++ server/server.js
@@ -33,6 +33,7 @@ app.get('/todos', (req,res) => {
 
 app.get('/todos/:id', (req, res) => {
   var id = req.params.id;
+
   if(!ObjectID.isValid(id)) {
     return res.status(404).send();
   }
@@ -42,7 +43,23 @@ app.get('/todos/:id', (req, res) => {
     }
     res.send({todo});
   }).catch((e) => {
-    return res.status(400).send(e);
+    return res.status(400).send();
+  });
+});
+
+app.delete('/todos/:id', (req, res) => {
+var id = req.params.id;
+
+if(!ObjectID.isValid(id)) {
+  return res.status(404).send();
+}
+Todo.findByIdAndRemove(id).then((todo) => {
+  if (!todo) {
+    return res.status(404).send();
+  }
+  res.send(todo);
+}).catch((e) => {
+   res.status(400).send();
   });
 });
 
