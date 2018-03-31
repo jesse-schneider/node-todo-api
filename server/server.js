065093dfcfb6531c34a405c6462b31a43e8c7ff8
Jesse Schneider <jessewschneider96@gmail.com>
2018-03-31 12:08:12 +1000

    Add get todos by id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo.js');
@@ -26,11 +27,24 @@ app.get('/todos', (req,res) => {
     res.send({todos});
   }, (e) => {
     res.status(400).send(e);
-  })
-
+  });
 });
 
-
+// GET /todos/1234234
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e) => {
+    return res.status(400).send(e);
+  })
+});
 
 app.listen(3000, () => {
   console.log('app started on port 3000')
